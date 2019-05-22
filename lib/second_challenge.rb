8051f6779f7b4e58e84c90a37d612dca345a73c3
Alexander Riccio <test35965@gmail.com>
2019-05-22 22:18:31 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => [{}]
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
@@ -13,7 +13,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => [{}]
+      :heroine_friends => []
       }}
 
   
